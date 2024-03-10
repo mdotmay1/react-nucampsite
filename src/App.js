0ddebee36a027877cc563e254a4be8b79ebec485
Mark May <markmay618@outlook.com>
2024-03-09 16:31:45 -1000

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 //import CampsitesList from './features/campsites/CampsitesList.js';   replaced with CampsiteDirectoryPage
@@ -11,8 +14,13 @@ import './App.css';
     return (
       <div className='App'>
           <Header />
-          {/* <CampsitesList /> */}
-          <CampsitesDirectoryPage />
+          {/* <CampsitesList /> REMOVED WHEN WE PUT IN THE CAPSITESDIRECTORYPAGE */}
+          {/* <CampsitesDirectoryPage /> REMOVED WHEN WE PUT IN ROUTES*/}
+          <Routes>
+              <Route path='/' element={<HomePage />} />
+              <Route path='contact' element={<ContactPage />} />
+              <Route path='directory' element={<CampsitesDirectoryPage />} />
+          </Routes>
           <Footer />
       </div>
     );
