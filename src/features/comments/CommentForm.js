6affcffaea682dd8230e60b360feaac9eede2d56
Mark May <markmay618@outlook.com>
2024-03-16 21:41:15 -1000

    Week 3 Worshop Task 2 Completed

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,53 +1,112 @@
 import { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
+import { Formik, Field, Form } from 'formik';
 
-/*
-    Create a function component named CommentForm. In its parameter list, destructure a campsiteId prop, using object destructuring syntax.
-    Make sure to export it as the default.
-*/
+
 
 const CommentForm = ({ campsiteId }) => {
     
-    /* In the component body, use array destructuring syntax to destructure modalOpen and setModalOpen from a call to the useState() hook. 
+    /* TASK 1: In the component body, use array destructuring syntax to destructure modalOpen and setModalOpen from a call to the useState() hook. 
        Initialize the modalOpen variable's value to false by passing that value as an argument to useState(). */
     const [modalOpen, setModalOpen] = useState(false);
 
+    /* TASK 2: 
+
+        Above the return statement of the CommentForm component, declare an arrow function named handleSubmit, using const.
+        Give it a single parameter of values.
+        In the arrow function body, declare a const named comment and initialize its value to the following object:
+        {
+            campsiteId: parseInt(campsiteId),
+            rating: values.rating,
+            author: values.author,
+            text: values.commentText
+        };
+        Below this, still inside the handleSubmit function body, log the comment object to the console.
+        Then call setModalOpen with a value of false to update the modalOpen state, which should have the effect of closing the modal at the end of this function's execution.
+
+    */
+    const handleSubmit = (values) => {
+        const comment =
+            {
+                campsiteId: parseInt(campsiteId),
+                rating: values.rating,
+                author: values.author,
+                text: values.commentText,
+            };
+            console.log(comment);
+            setModalOpen(false);
+    };
+
     return (
         <>
-            {/* 
-
-                Give the Button component a Boolean prop of outline and an onClick prop of an arrow function that returns the value from calling setModalOpen(true),
-                making sure to surround the arrow function in curly braces since you are using JavaScript inside JSX. The onClick prop should look like this:
-                onClick={() => setModalOpen(true)}
-            */}
              <Button outline onClick={() => setModalOpen(true)}><i className='fa fa-pencil fa-lg' /> Add Comment</Button> 
-             
+        
              {/* 
              
-             <Button outline onclick={() => setModalOpen(true)}>   --   MY INITIAL CODE.  USED TOGGLING MODALS CODE CHALLENGE AS A GUIDE.  
-                                                                        CLICKING BUTTON DID NOTHING.  DIDN'T CAPITALIZE "C" IN ONCLCK.
+             <Button outline onclick={() => setModalOpen(true)}>   --   MY INITIAL CODE.  CLICKING BUTTON DID NOTHING.  DIDN'T CAPITALIZE "C" IN ONCLCK.
   
             */}
-             
-             
-             
-             {/* 
-             
-                Beneath the Button, render a Modal component.
-                Give it a prop of isOpen={modalOpen}. This will cause the Modal to open or close depending on the value of modalOpen.
-                In the Modal component, render a ModalHeader component with a toggle prop.
-                For the value of this toggle prop, pass an arrow function that returns setModalOpen(false). Make sure to surround the arrow function in curly braces.
-                This will cause the modalOpen state to be set back to false when the Modal is closed.
-                Give the ModalHeader component a text content of Add Comment, between its start and end tags.
-                Below the ModalHeader component, render a ModalBody component that simply shows the current campsiteId, as follows:
-                <ModalBody>campsite: {campsiteId}</ModalBody> 
-            
-            */}
 
-            
              <Modal isOpen={modalOpen}>
                 <ModalHeader toggle={() => setModalOpen(false)}>Add Comment</ModalHeader>
-                    <ModalBody>campsite: {campsiteId}</ModalBody>
+                    <ModalBody>
+                        {/*campsite: {campsiteId}  removed in Task 2*/}
+                    
+                        <Formik 
+                            initialValues={{
+                                rating: undefined,
+                                author: '',
+                                commentText: ''
+                            }}
+                            onSubmit={handleSubmit}
+                        >
+
+                        <Form>
+                            <FormGroup>
+                                <Label htmlFor='rating'>
+                                    Rating
+                                </Label>
+                                <Field  
+                                    name='rating'
+                                    as='select'
+                                    className='form-control'
+                                >
+                                    <option>Select...</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option> 
+                                </Field>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor='author'>
+                                    Your Name
+                                </Label>
+                                <Field  
+                                    name='author'
+                                    placeholder='Your Name'
+                                    className='form-control'
+                                >
+                                </Field>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor='commentText'>
+                                    Comment
+                                </Label>
+                                <Field
+                                    name='commentText'
+                                    as='textarea'
+                                    rows='12'
+                                    className='form-control'
+                                />
+                            </FormGroup>
+                            <Button type='submit' color='primary'>
+                                Submit
+                            </Button>
+                        </Form>
+                        </Formik>
+                    </ModalBody> 
             </Modal>
         </>
 
--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -25,3 +25,4 @@ const CommentsList = ({ campsiteId }) => {
 };
 
 export default CommentsList;
+
