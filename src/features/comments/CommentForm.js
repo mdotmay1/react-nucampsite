286c07bbfb9013b291e43dc5597184e9a389da0f
Mark May <markmay618@outlook.com>
2024-03-16 22:27:35 -1000

    Week 3 Workshop Task 3 Completed

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { validateCommentForm } from '../../utils/validateCommentForm';
 
 
 
@@ -59,6 +60,7 @@ const CommentForm = ({ campsiteId }) => {
                                 commentText: ''
                             }}
                             onSubmit={handleSubmit}
+                            validate={validateCommentForm}
                         >
 
                         <Form>
@@ -78,6 +80,9 @@ const CommentForm = ({ campsiteId }) => {
                                     <option>4</option>
                                     <option>5</option> 
                                 </Field>
+                                <ErrorMessage name='rating'>
+                                    {(msg) => <p className='text-danger'>{msg}</p>}
+                                </ErrorMessage>
                             </FormGroup>
                             <FormGroup>
                                 <Label htmlFor='author'>
@@ -89,6 +94,9 @@ const CommentForm = ({ campsiteId }) => {
                                     className='form-control'
                                 >
                                 </Field>
+                                <ErrorMessage name='author'>
+                                    {(msg) => <p className='text-danger'>{msg}</p>}
+                                </ErrorMessage>
                             </FormGroup>
                             <FormGroup>
                                 <Label htmlFor='commentText'>
