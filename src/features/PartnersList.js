833180938ec3ac4d9d7f2cec3fd99bdc420c2d2a
Mark May <markmay618@outlook.com>
2024-03-21 22:44:37 -1000

    Campsites useSelector

--- src/features/PartnersList.js
+++ src/features/PartnersList.js
@@ -1,25 +1,44 @@
+import { useSelector } from 'react-redux';
 import { Col } from 'reactstrap';
 import Partner from './partners/Partner';
 
 import { selectAllPartners } from './partners/partnersSlice';
 
-const PartnersList = () => {
-    const partners = selectAllPartners();
+// const PartnersList = () => {
+//     const partners = selectAllPartners();
 
-    return (
-        <Col className='mt-4'>
-            {
-                partners.map((partner) => {
-                    return (
-                        <div className='d-flex mb-5' key={partner.id}>
-                            <Partner partner={partner} />
-                        </div>
-                    );
-                })
-            }
-        </Col>
-    )
-};
+//     return (
+//         <Col className='mt-4'>
+//             {
+//                 partners.map((partner) => {
+//                     return (
+//                         <div className='d-flex mb-5' key={partner.id}>
+//                             <Partner partner={partner} />
+//                         </div>
+//                     );
+//                 })
+//             }
+//         </Col>
+//     )
+// };
+
+const PartnersList = () => {
+        const partners = useSelector(selectAllPartners);
+    
+        return (
+            <Col className='mt-4'>
+                {
+                    partners.map((partner) => {
+                        return (
+                            <div className='d-flex mb-5' key={partner.id}>
+                                <Partner partner={partner} />
+                            </div>
+                        );
+                    })
+                }
+            </Col>
+        )
+    };
 
 export default PartnersList;
 
--- src/features/partners/partnersSlice.js
+++ src/features/partners/partnersSlice.js
@@ -1,30 +1,37 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { PARTNERS } from '../../app/shared/PARTNERS';
 
 
-/*
-
-FROM TASK 3:
-Next, export a const named selectAllPartners. Set the value for this to an arrow function that returns the entire PARTNERS array. 
-Look at campsitesSlice.js for a corresponding example.
-
-*/
-
-export const selectAllPartners = () => {
-    // return { PARTNERS }  // INITIAL WAY I DID IT.  WHEN DO I NEED CURLY BRACKETS AND WHEN DO I NOT?
-    return  PARTNERS;  
+const initialState = {
+    partnersArray: PARTNERS
 };
 
-/*
-    FROM TASK 3:
-    Next, export a const named selectFeaturedPartner. Set the value for this to an arrow function that finds the first partner with a 
-    truthy featured property from the PARTNERS array. You can use the selectFeaturedCampsite() and selectFeaturedPromotions() functions 
-    as examples of how to do this.
-*/
+const partnersSlice = createSlice({
+    name: 'partners',
+    initialState
+});
 
-export const selectFeaturedPartner = () => {
-    //PARTNERS.find((featured) => featured === featured); INITIAL WAY I DID IT.  STILL NEED MORE PRACTICE W/ ARROW FUNCTIONS
-    return PARTNERS.find((partner) => partner.featured);  
+export const partnersReducer = partnersSlice.reducer;
+
+
+// export const selectAllPartners = () => {
+//     return  PARTNERS;  
+// };
+
+export const selectAllPartners = (state) => {
+    return  state.partners.partnersArray;
 };
+
+
+// export const selectFeaturedPartner = () => {
+
+//     return PARTNERS.find((partner) => partner.featured);  
+// };
+
+export const selectFeaturedPartner = (state) => {
+    return state.partners.partnersArray.find((partner) => partner.featured); 
+};
+
 
 
 
