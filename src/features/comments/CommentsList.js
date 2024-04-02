c9c4241702daf6f75142ef5bd6eb852c3b5d01a8
Mark May <markmay618@outlook.com>
2024-04-01 18:25:06 -1000

    Workshop Week 5 task 2 completed

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,32 +1,33 @@
 import { useSelector } from 'react-redux';
-import { Col } from 'reactstrap';
+import { Col, Row } from 'reactstrap';
 import Comment from './Comment';
 import { selectCommentsByCampsiteId } from './commentsSlice';
 import CommentForm from './CommentForm';
+import Loading from '../../components/Loading';
+import Error from '../../components/Error';
 
-// const CommentsList = ({ campsiteId }) => {
-//     const comments = selectCommentsByCampsiteId(campsiteId);
 
-//     if (comments && comments.length > 0) {
-//         return (
-//             <Col md='5' className='m-1'>
-//                 <h4>Comments</h4>
-//                 {comments.map((comment) => {
-//                     return <Comment key={comment.id} comment={comment} />;
-//                 })}
-//                 <CommentForm campsiteId={campsiteId}/> 
-//             </Col>
-//         );
-//     }
-//     return (
-//         <Col md='5' className='m-1'>
-//             There are no comments for this campsite yet.
-//         </Col>
-//     );
-// };
 
 const CommentsList = ({ campsiteId }) => {
     const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
+    const isLoading = useSelector((state) => comments.isLoading);
+    const errMsg = useSelector((state) => comments.errMsg);
+
+    if (isLoading) {
+        return (
+            <Row>
+                <Loading />
+            </Row>
+        );
+    }
+
+    if (errMsg) {
+        return (
+            <Row>
+                <Error errMsg={errMsg} />
+            </Row>
+        );
+    }
 
     if (comments && comments.length > 0) {
         return (
